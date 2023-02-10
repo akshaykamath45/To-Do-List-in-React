4c0331697e35d3fef1320e62ffe504137c4f1853
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-11 03:35:07 +0530

    Marking the Task Completed
    Added a boolean variable completed to the task object assigning the default value as false.
    
    Created a function completeTask,using map function to check if the task is completed or not.
    
    Used props wherever required.
    
    Changed the color of the div to green,if the task is completed.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import {Task} from"./Task";
+import { Task } from "./Task";
 
 function App() {
   const [toDoList, setToDoList] = useState([]); //We use an array to store the tasks.
   const [newText, setNewText] = useState(""); //to get the input text for adding the task.
+
   const inputChangeHandler = (event) => {
     setNewText(event.target.value);
   };
+
   const addTask = () => {
-    const task={
-      id: toDoList.length===0 ? 1 : toDoList[toDoList.length-1]+1,
-      taskName: newText
+    const task = {
+      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1] + 1,
+      taskName: newText,
+      completed: false, //If the task is completed or not.
     };
     const newToDoList = [...toDoList, task]; //... is the spread operator.It means that we are copying the old array and adding the new task to it.
     setToDoList(newToDoList);
   };
+
   const deleteTask = (id) => {
     //Filter Function
     // const arr=["akshay","aditya","james"];
@@ -27,11 +31,25 @@ function App() {
     //   }
     // })
     const newToDoList = toDoList.filter((task) => {
-      return (task.id !== id); //It will return false if the task is equal to the taskName and true if it is not equal.Hence the false tasks will be filtered out/deleted.
+      return task.id !== id; //It will return false if the task is equal to the taskName and true if it is not equal.Hence the false tasks will be filtered out/deleted.
     });
     setToDoList(newToDoList);
     //setToDoList(toDoList.filter((task) =>{task !== taskName})-->Condensed version of the above code.
   };
+
+  const completeTask = (id) => {
+    setToDoList(
+      toDoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
+  
   return (
     <div className="App">
       <div className="addTask">
@@ -42,7 +60,14 @@ function App() {
       <div classname="list">
         {toDoList.map((task) => {
           return (
-            <Task taskName={task.taskName} id={task.id} deleteTask={deleteTask}/>//Passing deleteTask as a prop to Task.js-->Important
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+
+            /> //Passing deleteTask as a prop to Task.js-->Important
           );
         })}
       </div>
--- src/Task.js
+++ src/Task.js
@@ -1,8 +1,9 @@
 export const Task=(props)=>{
     return (
-        <div>
+        <div className="task" style={{backgroundColor: props.completed ? "green" : "white"}}>
           <h1>{props.taskName}</h1>
           <button onClick={() => props.deleteTask(props.id)}>X</button>
+          <button onClick={() => props.completeTask(props.id)}>Complete</button>
         </div>
       );
 }
