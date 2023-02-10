851bcdcd6e6a8e94865d82b62fd2a97616e9a43d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-11 02:56:40 +0530

    Deleting task [2]
    Previously we were differentiating with respect to the task name,but the issue was all the task name with same name would get deleted.
    
    Hence,We need to have some identifier to differentiate each task in the list. Therefore we use -->id : for each element in the list
    
    We consider each task as object consisting of taskName and id.
    
    While adding,we cannot consider new value to be string(text),it should be an object.
    
    So,we make the following changes :
    
    In the addTask function,we create an object of id,and taskName,and create a list according to the ID.
    
    Make the necessary changes in the main return statement as we are using the object consisting of id and taskName.
    
    In the deleteTask function,we pass the ID,and compare the ID to delete the task.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [toDoList,setToDoList]=useState([]);//We use an array to store the tasks.
-  const [newText,setNewText]=useState("");//to get the input text for adding the task.
-  const inputChangeHandler=(event)=>{
+  const [toDoList, setToDoList] = useState([]); //We use an array to store the tasks.
+  const [newText, setNewText] = useState(""); //to get the input text for adding the task.
+  const inputChangeHandler = (event) => {
     setNewText(event.target.value);
-  }
-  const addTask=()=>{
-    const newToDoList=[...toDoList,newText];//... is the spread operator.It means that we are copying the old array and adding the new task to it.
+  };
+  const addTask = () => {
+    const task={
+      id: toDoList.length===0 ? 1 : toDoList[toDoList.length-1]+1,
+      taskName: newText
+    };
+    const newToDoList = [...toDoList, task]; //... is the spread operator.It means that we are copying the old array and adding the new task to it.
     setToDoList(newToDoList);
-  }
-  const deleteTask=(taskName)=>{
+  };
+  const deleteTask = (id) => {
     //Filter Function
     // const arr=["akshay","aditya","james"];
     // const newArr=arr.filter((name)=>{
@@ -21,30 +25,27 @@ function App() {
     //     return true;
     //   }
     // })
-    const newToDoList=toDoList.filter((task)=>{
-      if(task===taskName){
-        return false;
-      }else{
-        return true;
-      }
-    })
+    const newToDoList = toDoList.filter((task) => {
+      return (task.id !== id); //It will return false if the task is equal to the taskName and true if it is not equal.Hence the false tasks will be filtered out/deleted.
+    });
     setToDoList(newToDoList);
-  }
+    //setToDoList(toDoList.filter((task) =>{task !== taskName})-->Condensed version of the above code.
+  };
   return (
     <div className="App">
       <div className="addTask">
-        <input type="text" onChange={inputChangeHandler}/>
+        <input type="text" onChange={inputChangeHandler} />
         <button onClick={addTask}>Add Task</button>
       </div>
 
       <div classname="list">
-        {toDoList.map((task)=>{
-             return(
-                <div>
-                <h1>{task}</h1>
-                <button onClick={()=>deleteTask(task)}>X</button>
-              </div>
-             );
+        {toDoList.map((task) => {
+          return (
+            <div>
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)}>X</button>
+            </div>
+          );
         })}
       </div>
     </div>
